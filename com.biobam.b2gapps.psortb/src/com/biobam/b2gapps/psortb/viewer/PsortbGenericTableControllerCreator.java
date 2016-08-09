5a33b5d25a52b8abbf5267f879b8d28f2848f65d
Robert Nica <robynic@gmail.com>
2016-08-09 20:06:53 +0200

    Change to the new viewer controller API

--- com.biobam.b2gapps.psortb/src/com/biobam/b2gapps/psortb/viewer/PsortbGenericTableControllerCreator.java
+++ com.biobam.b2gapps.psortb/src/com/biobam/b2gapps/psortb/viewer/PsortbGenericTableControllerCreator.java
@@ -1,8 +1,11 @@
 package com.biobam.b2gapps.psortb.viewer;
 
+import java.util.EnumSet;
+
 import com.biobam.b2gapps.psortb.data.PsortbObject;
 import com.biobam.blast2go.api.dataviewer.interfaces.IB2GObjectController;
 import com.biobam.blast2go.api.dataviewer.interfaces.IB2GObjectControllerCreator;
+import com.biobam.blast2go.api.dataviewer.interfaces.WorkbenchProperty;
 
 public class PsortbGenericTableControllerCreator implements IB2GObjectControllerCreator<PsortbObject> {
 
@@ -19,6 +22,21 @@ public class PsortbGenericTableControllerCreator implements IB2GObjectController
 	@Override
 	public IB2GObjectController<PsortbObject> getControllerInstance(PsortbObject object) {
 		return new PsortbGenericTableController(object);
+	}
+
+	@Override
+	public int getPreferredPositionInMenu() {
+		return 0;
+	}
+
+	@Override
+	public String getViewerName() {
+		return "Table";
+	}
+
+	@Override
+	public EnumSet<WorkbenchProperty> getWorkbenchProperties() {
+		return EnumSet.of(WorkbenchProperty.DEFAULT_VIEWER, WorkbenchProperty.IMPORTANCE_PRIMARY_VIEWER, WorkbenchProperty.POSITION_MAIN, WorkbenchProperty.SAVE_ALLOWED);
 	}
 
 }
